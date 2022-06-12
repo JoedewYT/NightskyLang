04ba8d3b282984c59fee9824f604899c6262e915
JoedewYT <joeveraart@gmail.com>
2022-06-12 11:45:03 +0200

    working on lexer.

--- src/include/lexer.h
+++ src/include/lexer.h
@@ -1,5 +1,8 @@
 #ifndef LEXER_H
 #define LEXER_H
+#include "token.h"
+
+
 typedef struct LEXER_STRUCT
 {
     char c;
@@ -12,4 +15,10 @@ lexer_T* init_lexer(char* contents);
 void lexer_advance(lexer_T* lexer);
 
 void lexer_skip_whitespace(lexer_T* lexer);
+
+token_T* lexer_get_next_token(lexer_T* lexer);
+
+token_T* lexer_collect_string(lexer_T* lexer);
+
+char* lexer_get_current_char_as_string(lexer_T* lexer);
 #endif
